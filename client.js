40ab5a292cbb556feab3c021b891ae66b2d943c9
Garrett Baker <garrett.baker@gmail.com>
2015-09-05 20:54:52 -0400

    refining api registration

--- client.js
+++ client.js
@@ -2,10 +2,17 @@ var net = require( 'net' );
 var rax = require( 'rax-api' );
 
 module.exports = function( config, options ){
-
   if ( !options || !options.api || typeof options.api !== 'object' ) {
     return;
   }
 
-  rax( config ).queues.publish( 'API-Reg', options.api );
+  rax( config ).queues.publish( 'API-Reg', options.api, function( err, payload ){
+    if ( err ) {
+      console.error( err );
+      return;
+    }
+
+    console.log( 'API registration message posted.' );
+    console.log( "Response: \n %s", JSON.stringify( payload ) );
+  });
 };
